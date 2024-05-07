aee92293268f8024799d4ebd57fdb1116b6343d5
xcheche <chekwubechukwu23@gmail.com>
2024-05-07 04:42:19 +0100

    templete-inheritance added

--- relecloud/views.py
+++ relecloud/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+
+from relecloud.models import Destination
 # Create your views here.
 def index(request):
     return render(request,'relecloud/index.html')
@@ -8,3 +10,8 @@ def index(request):
 def about(request):
     date = datetime.datetime.now()
     return render(request,'relecloud/about.html',{'date':date})
+
+
+def destination(request):
+    all_destination = Destination.objects.all()
+    return render(request,'relecloud/destination.html',{'destinations':all_destination})
